52b8ad2aef5910241f7af8339271066b3a94f0c6
Roger Schmidlin <schmiddyman@gmail.com>
2016-07-20 09:51:19 +0800

    fresh newly created project

---
+++ RulesOfSquash/RulesOfSquashPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace RulesOfSquash
+{
+	public partial class RulesOfSquashPage : ContentPage
+	{
+		public RulesOfSquashPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
